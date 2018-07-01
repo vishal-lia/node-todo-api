8db16babf5f1cbb189a35f77ed8b13dc36d6f4a0
krunal-lia <krunallia18@gmail.com>
2018-07-01 22:39:04 +0530

    Add Test case for DELETE /todo/:id

--- server/server.js
+++ server/server.js
@@ -61,7 +61,7 @@ app.delete('/todos/:id', (req, res) => {
             res.status(404).send();
         }
 
-        res.send(todo);
+        res.send({todo});
     }).catch(err => {
         res.status(400).send();
     });
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -100,3 +100,40 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove todo', done => {
+        let hexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo.text).toBe(todos[1].text);
+            })
+            .end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+
+                Todo.findById(hexId).then(todo => {
+                    expect(todo).toBeFalsy();
+                    done();
+                }).catch(err => done(err));
+            });
+    });
+
+    it('should return 404 if ID not found', done => {
+        request(app)
+            .delete(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 for non-object ID\'s', done => {
+        request(app)
+            .delete('/todos/123')
+            .expect(404)
+            .end(done);
+    });
+});
