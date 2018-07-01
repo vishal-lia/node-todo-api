207666e1d7d13447b66646af40380381c969736f
krunal-lia <krunallia18@gmail.com>
2018-07-01 19:39:38 +0530

    Setup app for Heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+const MONGODB_URI = 'mongodb://vishal_lia:vishal786@ds121960.mlab.com:21960/nodedb';
+//const MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+
+mongoose.connect(MONGODB_URI);
 
 module.exports = { mongoose };
