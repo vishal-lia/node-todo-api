051ea8ff7cb1dfc806a86b303f23d1c6e838179c
krunal-lia <krunallia18@gmail.com>
2018-07-01 21:58:03 +0530

    Add DELETE /todos/:id endpoint

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.get('/todos/:id', (req, res) => {
             res.status(404).send();
         }
 
-        res.status(200).send({todo});
+        res.send({todo});
     }).catch(err => {
         res.status(400).send();
     });
@@ -46,6 +46,24 @@ app.post('/todos', (req, res) => {
         res.send(doc);
     }).catch(err => {
         res.status(400).send(err);
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then(todo => {
+        if(!todo) {
+            res.status(404).send();
+        }
+
+        res.send(todo);
+    }).catch(err => {
+        res.status(400).send();
     });
 });
 
